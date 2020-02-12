56aeeda349aa5142231fab49f7adac00dc847cd7
Mike Jiang <60728792+mikejiang110@users.noreply.github.com>
2020-02-12 18:38:35 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+//Click to change background-image
 $('.Color').on('click', function() {
 	var backgroundPictureUrl = $(this).attr('data-background')
 	console.log(backgroundPictureUrl)
 	$('body').css('background-image', `url(${backgroundPictureUrl})`)
 })
+//Use click function to change the opacity in CSS to show the answers
 $('#answer').on('click', function() {
 	$('.rightanswer').css('opacity', '1')
 })
+// Reload the page
 $('#reload').on('click', function() {
 	location.reload();
 })
